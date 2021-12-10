ef1cc765a182a756eaba7d7428c3fad3a245f7d4
Tony <ag04498118@edu.mon.bg>
2021-12-10 23:42:31 +0200

    Fixed typos

--- pages/index.js
+++ pages/index.js
@@ -12,7 +12,7 @@ export default function Home() {
         <h2>About me</h2>
            <p>Hello, I'm Tony. I am a Bulgarian student that recently took up coding. I make discord bots using <a href="https://discord.js.org/">discord.js</a> and javascript/typescript, and in most of my free time I moderate different communities on discord. Drop me a follow on <a href="https://github.com/tonyG433">GitHub</a> and on <a href='https://twitter.com/I_am_tonyg'>Twitter</a>, to contact me you can send me a friend reqest on discord, my name on there is tonyG#0001</p>
         <h2>Why this blog?</h2>
-            <p>I was messing around with <a href='https://nextjs.org/'>Next.js</a> and I saw a simple step-by-step <a href='https://nextjs.org/learn'>blog tutorial</a> so i decided to give it a go. Here you will find all sorts of things that were written by me. Some articles may be bad, some may be good but you guys decide. My goal is to upload a new article at least once every weekl, not promissing I will, more like an upload goal. Anyway, you can read my articles down below, thanks!</p>
+            <p>I was messing around with <a href='https://nextjs.org/'>Next.js</a> and I saw a simple step-by-step <a href='https://nextjs.org/learn'>blog tutorial</a> so i decided to give it a go. Here you will find all sorts of things that were written by me. Some articles may be bad, some may be good but you guys decide. My goal is to upload a new article at least once every week, not promissing, more like an upload goal. Anyway, you can read my articles down below, thanks!</p>
       </section>
     </Layout>
   )
